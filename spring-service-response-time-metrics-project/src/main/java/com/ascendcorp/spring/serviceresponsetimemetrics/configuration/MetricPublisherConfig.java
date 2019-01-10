c618bc42dad1d186ea24409c8f9e7ae26ed28ebe
Sowat Tuntra-umpai <sowat.tun@ascendcorp.com>
2019-01-10 15:55:21 +0700

    Fix wrong update response time

--- spring-service-response-time-metrics-project/src/main/java/com/ascendcorp/spring/serviceresponsetimemetrics/configuration/MetricPublisherConfig.java
+++ spring-service-response-time-metrics-project/src/main/java/com/ascendcorp/spring/serviceresponsetimemetrics/configuration/MetricPublisherConfig.java
@@ -1,28 +1,31 @@
 package com.ascendcorp.spring.serviceresponsetimemetrics.configuration;
 
-
 import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.Tags;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.ToDoubleFunction;
 
 public class MetricPublisherConfig {
 
     private static String NAME_RESPONSE_TIME_MS = "spring_response_time_ms";
 
     private static MetricPublisherConfig instance = new MetricPublisherConfig();
-
-    private static MetricInfo metricInfo;
+    private static Map<String, MetricInfo> metricInfoMap = new HashMap();
 
     public MetricPublisherConfig() {
         System.out.println("MetricPublisherConfig created");
     }
 
     public static void publish(MetricInfo metricInfo) {
-        MetricPublisherConfig.metricInfo = metricInfo;
+        metricInfoMap.put(metricInfo.getEndpoint(), metricInfo);
+        ToDoubleFunction x = (value) -> instance.getDurationInMilliSec(metricInfo.getEndpoint());
 
-        Metrics.gauge(NAME_RESPONSE_TIME_MS, Tags.of("status", Integer.toString(metricInfo.getHttpStatus()), "uri", metricInfo.getEndpoint()), instance, MetricPublisherConfig::getDurationInMilliSec);
+        Metrics.gauge(NAME_RESPONSE_TIME_MS, Tags.of("status", Integer.toString(metricInfo.getHttpStatus()), "uri", metricInfo.getEndpoint()), instance, x);
     }
 
-    private long getDurationInMilliSec() {
-        return MetricPublisherConfig.metricInfo.getDurationInMilliSec();
+    private Long getDurationInMilliSec(String endpoint) {
+        return metricInfoMap.get(endpoint).getDurationInMilliSec();
     }
 }
